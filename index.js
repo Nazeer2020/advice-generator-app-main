99f33ea231335f9428dd70256538c43d40bde466
Nazeer Haidari <nazeer.haidari@gmail.com>
2022-03-23 20:06:48 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const text = document.querySelector(".text");
+const id = document.querySelector(".id");
+const btn = document.querySelector(".btn");
+
+async function getAdvice() {
+  const randomNumber = Math.floor(Math.random() * 118);
+  try {
+    const data = await fetch(
+      `https://api.adviceslip.com/advice/${randomNumber}`
+    );
+    const res = await data.json();
+
+    updatingUI(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function updatingUI(res) {
+  text.innerText = `"${res.slip.advice}"`;
+  id.innerText = `Advice #${res.slip.id}`;
+}
+
+btn.addEventListener("click", getAdvice);
